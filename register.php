688c008656a52cb963a59a1bd548638cafd9129d
blunqa <tm.blom.94@gmail.com>
2015-11-13 10:34:53 +0200

    asd

---
+++ register.php
@@ -0,0 +1,19 @@
+<form>
+    Etunimi: <input type="text" name="etunimi"><br/>
+    Sukunimi: <input type="text" name="sukunimi"><br/>
+    Syntymäaika: <br/>
+    Katuosoite: <input type="text" name="katuosoite"><br/>
+    Postinumero ja kaupunki: <input type="text" name="postcity"><br/>
+    <hr/>
+    Sukupuoli: <select name="gender">
+    		<option value="" disabled selected>Select your option</option>
+            <option value="mies">Mies</option>
+            <option value="nainen">Nainen</option>
+            <option value="null">En kerro fug u</option>
+            </select><br/>
+    
+    Koulutus: <input type="text" name="koulutus"><br/>
+    Käyttämäsi ohjelmisto/välineistö: <input type="text" name="equipment">
+
+
+</form>
