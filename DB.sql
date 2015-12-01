c67a2e58dd2e74c0f0d8ae16b0752051b568b849
blunqa <tm.blom.94@gmail.com>
2015-12-01 10:12:39 +0200

    Create DB.sql

---
+++ DB.sql
@@ -0,0 +1,34 @@
+CREATE TABLE KAYTTAJA(
+    id INT PRIMARY KEY,
+    username CHAR(30),
+    saika DATE,
+    sposti CHAR(50),
+    salasana CHAR(30)
+);
+
+CREATE TABLE PROFIILI(
+    id INT PRIMARY KEY,
+    etunimi CHAR(25),
+    sukunimi CHAR(25),
+    pnumero CHAR(25),
+    sposti CHAR(50),
+    osoite CHAR(30),
+    kaupunki CHAR(30),
+    postinumero CHAR(10),
+    maa CHAR(30,),
+    omistaja INT,
+    FOREIGN KEY(sposti) REFERENCES KAYTTAJA(sposti),
+    FOREIGN KEY(omistaja) REFERENCES KAYTTAJA(id)
+);
+
+CREATE TABLE PROJEKTI(
+    id INT PRIMARY KEY,
+    nimi CHAR(30),
+    rooli CHAR(100),
+    kuvaus CHAR(100),
+    url CHAR(100),
+    kuva CHAR(100),
+    avainsanat CHAR(100),
+    omistaja INT,
+    FOREIGN KEY(omistaja) REFERENCES KAYTTAJA(id)
+);
