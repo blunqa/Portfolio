82e02956f4eb348553abb13c7b7421861092301a
blunqa <tm.blom.94@gmail.com>
2015-12-04 12:44:33 +0200

    Update DB.sql

--- DB.sql
+++ DB.sql
@@ -1,47 +1,49 @@
+
+
 CREATE TABLE KAYTTAJA(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     username CHAR(30),
-    saika DATE,
     sposti CHAR(50),
     salasana CHAR(30)
-);
-
-CREATE TABLE PROFIILI(
-    id INT PRIMARY KEY,
     etunimi CHAR(25),
     sukunimi CHAR(25),
+    saika DATETIME,
     pnumero CHAR(25),
-    sposti CHAR(50),
     osoite CHAR(30),
     kaupunki CHAR(30),
     postinumero CHAR(10),
     maa CHAR(30,),
-    omistaja INT,
-    FOREIGN KEY(sposti) REFERENCES KAYTTAJA(sposti),
-    FOREIGN KEY(omistaja) REFERENCES KAYTTAJA(id)
+    sukupuoli CHAR(30),
+    koulutus CHAR(100),
+    ohj_val CHAR(100),
 );
+CREATE TABLE PROF_PROJ (
+    kayttaja INT,
+    projekti INT,
+    FOREIGN KEY(kayttaja) REFERENCES KAYTTAJA(id),
+    FOREIGN KEY(projekti) REFERENCES PROJEKTI(id)
+    );
 
 CREATE TABLE PROJEKTI(
-    id INT PRIMARY KEY,
+    id INT PRIMARY KEY AUTO_INCREMENT,
     nimi CHAR(30),
     rooli CHAR(100),
     kuvaus CHAR(100),
-    url CHAR(100),
     kuva CHAR(100),
-    avainsanat CHAR(100),
+    kategoria CHAR(100),
     omistaja INT,
     FOREIGN KEY(omistaja) REFERENCES KAYTTAJA(id)
 );
 
 CREATE TABLE PROJEKTI_KUVAT (
-    PROJEKTI INT,
-    KUVA INT,
-    FOREIGN KEY(PROJEKTI) REFERENCES PROJEKTI(id),
-    FOREIGN KEY(KUVA) REFERENCES KUVAT(ID)
+    projekti INT,
+    kuva INT,
+    FOREIGN KEY(projekti) REFERENCES PROJEKTI(id),
+    FOREIGN KEY(kuva) REFERENCES KUVAT(id)
     );
     
 CREATE TABLE KUVAT (
-    ID INT PRIMARY KEY,
-    PVM DATETIME,
-    KUVAUS CHAR(100)
+    id INT PRIMARY KEY,
+    pvm DATETIME,
+    kuvaus CHAR(100)
     );
