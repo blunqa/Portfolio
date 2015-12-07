6b0e86913a5b39a7eed5a5a92468871862800212
elmahone <mahonen93@gmail.com>
2015-12-07 12:16:29 +0200

    Update birth.php

--- birth.php
+++ birth.php
@@ -1,19 +1,3 @@
-<select name="DOBMonth">
-	<option> - Kuukausi - </option>
-	<option value="Tammikuu">Tammikuu</option>
-	<option value="Helmikuu">Helmikuu</option>
-	<option value="Maaliskuu">Maaliskuu</option>
-	<option value="Huhtikuu">Huhtikuu</option>
-	<option value="Toukokuu">Toukokuu</option>
-	<option value="Kesäkuu">Kesäkuu</option>
-	<option value="Heinäkuu">Heinäkuu</option>
-	<option value="Elokuu">Elokuu</option>
-	<option value="Syyskuu">Syyskuu</option>
-	<option value="Lokakuu">Lokakuu</option>
-	<option value="Marraskuu">Marraskuu</option>
-	<option value="Joulukuu">Joulukuu</option>
-</select>
-
 <select name="DOBDay">
 	<option> - Päivä - </option>
 	<?php
@@ -21,6 +5,21 @@
         echo '<option value="'. $i . '">' . $i . '</option>';
         }
         ?>
+</select>
+<select name="DOBMonth">
+	<option> - Kuukausi - </option>
+	<option value="1">Tammikuu</option>
+	<option value="2">Helmikuu</option>
+	<option value="3">Maaliskuu</option>
+	<option value="4">Huhtikuu</option>
+	<option value="5">Toukokuu</option>
+	<option value="6">Kesäkuu</option>
+	<option value="7">Heinäkuu</option>
+	<option value="8">Elokuu</option>
+	<option value="9">Syyskuu</option>
+	<option value="10">Lokakuu</option>
+	<option value="11">Marraskuu</option>
+	<option value="12">Joulukuu</option>
 </select>
 
 <select name="DOBYear">
