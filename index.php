36711c557e5a2d6444a453f3666c607d41dcd183
jasusetala <jasusetala@users.noreply.github.com>
2015-12-07 12:49:45 +0200

    Update index.php

--- index.php
+++ index.php
@@ -40,6 +40,27 @@
     </div>
         </div>
       <footer><p>(C) Kaikki oikeudet pidätetään Lumnium 2015 </p><p>| Usein Kysytyt Kysymykset | Ota yhteyttä | <a href="https://en.wikipedia.org/wiki/Steve_Jobs">Jobs</a> |</p></footer>
+      <!-- kirjautuminen -->
+	<div class="user modal" id="log-modal" style="display: block;">
+	    <a href="#" class="close-modal">×</a>
+	    <div class="user-login">
+	        Kirjaudu sisaan.
+	        <br>
+	        <form action="#" method="post">
+	            <input class="lomake" type="text" placeholder="username" name="username">
+	            <br><br>
+	            <input class="lomake" type="password" placeholder="Password" name="pwd">
+	            <br><br>
+	            <input type="submit" value="Kirjaudu">
+	        </form>
+	    </div>
+	
+	    <div class="user-sign">
+	        <br>
+	        Eiko sinulla ole viela tunnuksia? <a href="./">Rekisteröidy.</a>
+	    </div>
+	</div>
+	<!-- kirjautuminen päättyy -->
       </div>
 </body>
 </html>
