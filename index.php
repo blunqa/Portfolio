ceed848666237b49a1872d2a9757b9d8c9140295
elmahone <mahonen93@gmail.com>
2015-12-07 11:54:51 +0200

    Create index.php

---
+++ index.php
@@ -0,0 +1,45 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+  <title>portfoil</title>
+    <link href="font/stylesheet.css" rel="stylesheet" type="text/css">
+	<link href="app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="sivu">
+  <header>
+ <a href="index.php"><img src="img/logo.jpg" alt="Logo"></a>
+  <div class="top-block">
+		<nav class="top-nav">
+			<ul>
+				<li>
+					<a href="#" class="selaa">Selaa</a>
+				</li>
+				<li>
+					<a href="#" class="open-modal kirjaudu">Kirjaudu</a>
+				</li>				
+			</ul>
+		</nav>
+
+      </div>
+  </header>
+    <div class="esittely">
+        <div class="esittely-bg">
+       <div class="esittely-text">
+        <h1>Esittelyy</h1>
+           <p>Portfolio on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi. </br><b>Rekisteröidy jo tänään!</b></p>
+        </div><div class="esittely-reg">
+        <form class="reg-form">
+			<input class="reg-field lomake" type="text" name="user" placeholder="Username">
+            <input class="reg-field lomake" type="text" name="email" placeholder="Email">
+            <input class="reg-field lomake" type="password" name="pass" placeholder="Password">
+        </form>
+        <input type="submit" value="Rekisteröidy" id="reg">
+        </div>
+    </div>
+        </div>
+      <footer><p>(C) Kaikki oikeudet pidätetään Lumnium 2015 </p><p>| Usein Kysytyt Kysymykset | Ota yhteyttä | <a href="https://en.wikipedia.org/wiki/Steve_Jobs">Jobs</a> |</p></footer>
+      </div>
+</body>
+</html>
