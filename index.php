9d1a75eeec78ae0c92ba9bfd732d74a08007e541
elmahone <mahonen93@gmail.com>
2015-12-07 11:55:23 +0200

    Update index.php

--- index.php
+++ index.php
@@ -31,9 +31,9 @@
            <p>Portfolio on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi. </br><b>Rekisteröidy jo tänään!</b></p>
         </div><div class="esittely-reg">
         <form class="reg-form">
-			<input class="reg-field lomake" type="text" name="user" placeholder="Username">
-            <input class="reg-field lomake" type="text" name="email" placeholder="Email">
-            <input class="reg-field lomake" type="password" name="pass" placeholder="Password">
+		<input class="reg-field lomake" type="text" name="user" placeholder="Username">
+            	<input class="reg-field lomake" type="text" name="email" placeholder="Email">
+            	<input class="reg-field lomake" type="password" name="pass" placeholder="Password">
         </form>
         <input type="submit" value="Rekisteröidy" id="reg">
         </div>
