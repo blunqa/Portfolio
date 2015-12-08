110eb93d5d58dd684d80f0b6746107e89283386d
elmahone <mahonen93@gmail.com>
2015-12-08 09:50:26 +0200

    Update luoPortfolio.php

--- luoPortfolio.php
+++ luoPortfolio.php
@@ -1,30 +1,4 @@
-<html>
-<head>
-  <meta charset="UTF-8">	
-  <title>portfoil</title>
-	<link href="app.css" rel="stylesheet" type="text/css">
-</head>
-    <body>
-            <div id="sivu">
-        <header>
- <a href="index.php"><img src="img/logo.jpg" alt="Logo"></a>
-  <div class="top-block">
-		<nav class="top-nav">
-			<ul>
-				<li>
-					<a href="#" class="selaa">Selaa</a>
-				</li>
-				<li>
-					<a href="#" class="open-modal" id="kirjaudu">Kirjaudu</a>
-				</li>				
-			</ul>
-		</nav>
-		<form class="search-form">
-			<input class="search-field" type="text" name="haku" placeholder="Haku">
-			<button class="search-button" type="submit"><i class="fi-magnifying-glass large"></i></button>
-		</form>
-      </div>
-  </header>
+<?php include("includes/iheader.php"); ?>
         
         <p>Upload</p>
             <form action="upload.php" method="post" enctype="multipart/form-data">
@@ -33,6 +7,4 @@
     <input type="submit" value="Upload Image" name="submit">
 </form>    
                 
-        </div>
-    </body>
-</html>
+<?php include("includes/ifooter.php"); ?>
