9e1e58ba9a7b77f25a8acd78a6ee4b0c7dffb233
elmahone <mahonen93@gmail.com>
2015-12-08 10:25:13 +0200

    Create iheader.php

---
+++ iheader.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html>
+<head>
+    <meta charset="UTF-8">
+  <title>portfoil</title>
+    <link href="font/stylesheet.css" rel="stylesheet" type="text/css">
+	<link href="app.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div id="sivu">
+  <header>
+ <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
+  <div class="top-block">
+		<nav class="top-nav">
+			<ul>
+				<li>
+					<a href="selaa.php" class="selaa">Selaa</a>
+				</li>
+				<li>
+					<a href="#" class="open-modal kirjaudu">Kirjaudu</a>
+				</li>				
+			</ul>
+		</nav>
+
+      </div>
+  </header>
