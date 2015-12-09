5ba009dd971f2e7260db5218e3bf95512a347108
elmahone <mahonen93@gmail.com>
2015-12-09 10:31:40 +0200

    Create projekti.php

---
+++ projekti.php
@@ -0,0 +1,14 @@
+<?php include("includes/iheader.php"); ?>
+<div id="content">
+    <div id="tiedostot"><h1>Projektin tiedostot</h1><br><img src="uploads/2.jpg"></div>
+    <div id="info">
+        <div class="projekti-nimi"><h1>PLACEHOLDER NIMI</h1></div>
+        <div class="projekti-pvm">2.2.1999</div>
+        <div class="projekti-tekija"> PLACEHOLDER TEKIJÄ</div>
+        <div class="projekti-rooli"> PLACEHOLDER ROOLI</div>
+        <div class="projekti-share">FB | TWTR | G+</div>
+        <div class="projekti-kuvaus">PLACEHOLDER KUVAUS</div>
+    </div>
+
+</div>
+<?php include("includes/ifooter.php"); ?>
