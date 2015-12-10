e59d701dde667d3bdeb782e2a1781b7af5946c71
jasusetala <jasusetala@users.noreply.github.com>
2015-12-10 10:33:52 +0200

    Update iheader.php

--- iheader.php
+++ iheader.php
@@ -7,6 +7,15 @@
 	<link href="app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
+    <!-- Load Facebook SDK for JavaScript -->
+    <div id="fb-root"></div>
+    <script>(function(d, s, id) {
+      var js, fjs = d.getElementsByTagName(s)[0];
+      if (d.getElementById(id)) return;
+      js = d.createElement(s); js.id = id;
+      js.src = "//connect.facebook.net/fi_FI/sdk.js#xfbml=1&version=v2.5";
+      fjs.parentNode.insertBefore(js, fjs);
+    }(document, 'script', 'facebook-jssdk'));</script>
     <div id="sivu">
   <header>
  <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
