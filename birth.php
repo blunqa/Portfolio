b87ee4136bc9e72d298946cd6d4a420914ee5285
blunqa <tm.blom.94@gmail.com>
2015-12-10 12:51:28 +0200

    Update birth.php

--- birth.php
+++ birth.php
@@ -1,28 +1,32 @@
-<select name="DOBDay">
+<select name="data[day]" required>
 	<option> - Päivä - </option>
 	<?php
-        for ($i = 1; $i <= 31; $i++) {
+        for ($i = 1; $i <= 9; $i++) {
+        echo '<option value="'. "0" . $i . '">' . $i . '</option>';
+        }
+        for ($i = 10; $i <= 31; $i++) {
         echo '<option value="'. $i . '">' . $i . '</option>';
         }
         ?>
+    
 </select>
-<select name="DOBMonth">
+<select name="data[month]"  required>
 	<option> - Kuukausi - </option>
-	<option value="1">Tammikuu</option>
-	<option value="2">Helmikuu</option>
-	<option value="3">Maaliskuu</option>
-	<option value="4">Huhtikuu</option>
-	<option value="5">Toukokuu</option>
-	<option value="6">Kesäkuu</option>
-	<option value="7">Heinäkuu</option>
-	<option value="8">Elokuu</option>
-	<option value="9">Syyskuu</option>
+	<option value="01">Tammikuu</option>
+	<option value="02">Helmikuu</option>
+	<option value="03">Maaliskuu</option>
+	<option value="04">Huhtikuu</option>
+	<option value="05">Toukokuu</option>
+	<option value="06">Kesäkuu</option>
+	<option value="07">Heinäkuu</option>
+	<option value="08">Elokuu</option>
+	<option value="09">Syyskuu</option>
 	<option value="10">Lokakuu</option>
 	<option value="11">Marraskuu</option>
 	<option value="12">Joulukuu</option>
 </select>
 
-<select name="DOBYear">
+<select name="data[year]"  required>
 	<option> - Vuosi - </option>
 	<?php
         for ($i = 1940; $i <= 2015; $i++) {
