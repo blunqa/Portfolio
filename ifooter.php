57cd0c9cb77540656cfea4ac9fec36257d02bfa5
elmahone <mahonen93@gmail.com>
2015-12-10 12:48:52 +0200

    Update ifooter.php

--- ifooter.php
+++ ifooter.php
@@ -1,16 +1,19 @@
-      <footer><p>(C) Kaikki oikeudet pidätetään Lumnium 2015 </p><p>| Usein Kysytyt Kysymykset | Ota yhteyttä | <a href="https://en.wikipedia.org/wiki/Steve_Jobs">Jobs</a> |</p>
-       <br>
-        <?php include("fbShare.php"); ?></footer>
+  <footer><p>(C) Kaikki oikeudet pidätetään Lumnium 2015 </p><p>| Usein Kysytyt Kysymykset | Ota yhteyttä | <a href="https://en.wikipedia.org/wiki/Steve_Jobs">Jobs</a> |</p></footer>
       <!-- kirjautuminen -->
-	<div class="user modal" id="log-modal" style="display: none;">
+          <br>
+<?php include("fbShare.php"); ?></footer>
+<?php
+//Jos ei kirjauduttu sisään modaali tulee esiin
+if($_SESSION['logged_in'] != 'yes') {?>
+<div class="user modal" id="log-modal" style="display: none;">
 	    <a href="#" class="close-modal">×</a>
 	    <div class="user-login">
 	        Kirjaudu sisaan.
 	        <br>
-	        <form action="#" method="post">
-	            <input class="lomake" type="text" placeholder="username" name="username">
+	        <form action="login.php" method="post">
+	            <input class="lomake" type="text" placeholder="username" name="username" required>
 	            <br><br>
-	            <input class="lomake" type="password" placeholder="Password" name="pwd">
+	            <input class="lomake" type="password" placeholder="Password" name="salasana" required>
 	            <br><br>
 	            <input class="button_small" type="submit" value="Kirjaudu">
 	        </form>
@@ -21,7 +24,13 @@
 	        Eiko sinulla ole viela tunnuksia? <a href="./">Rekisteröidy.</a>
 	    </div>
 	</div>
+<?php } ?>
 	<!-- kirjautuminen päättyy -->
       </div>
+    <script type="text/javascript" src="//form-serialize.googlecode.com/svn/trunk/serialize-0.2.min.js"></script>
+	<script src="app.js"></script>
+      
 </body>
 </html>
+
+
