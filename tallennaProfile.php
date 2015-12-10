551215bacf002ccf83d3bb3044e67dfabb407f5f
blunqa <tm.blom.94@gmail.com>
2015-12-10 12:48:55 +0200

    Create tallennaProfile.php

---
+++ tallennaProfile.php
@@ -0,0 +1,44 @@
+<?php
+require_once('config.php');
+require_once('functions.php');
+SSLon();
+$userdata = unserialize($_SESSION['lomakedata']);  //tekstimuodosta takaisin taulukoksi
+$username = $_SESSION['username'];
+
+	try {
+			$STH2 = $DBH->prepare("UPDATE KAYTTAJA SET etunimi=:etunimi, sukunimi=:sukunimi, saika=:year-:month-:day, puh=:puh, osoite=:osoite, kaupunki=:kaupunki, postinumero=:postinumero, sukupuoli=:sukupuoli, koulutus=:koulutus, ohj_val=:ohj_val WHERE username = '$username';");
+            
+			if($STH2->execute($userdata)){
+					try { 
+					//Jos käyttäjän tallennus onnistui asetetaan hänet loggautuneeksi 
+					//eli kirjoitetaan käyttäjätiedot myös sessiomuuttujiin	
+					$sql = "SELECT * FROM KAYTTAJA WHERE username = '$username';";
+					$STH3 = $DBH->query($sql);
+					$STH3->setFetchMode(PDO::FETCH_OBJ);
+					$user = $STH3->fetch();
+					$_SESSION['etunimi'] = $user->etunimi;
+					$_SESSION['sukunimi'] = $user->sukunimi;
+                    $_SESSION['saika'] = $user->saika;
+                    $_SESSION['puh'] = $user->puh;
+                    $_SESSION['osoite'] = $user->osoite;
+                    $_SESSION['kaupunki'] = $user->kaupunki;
+                    $_SESSION['postinumero'] = $user->postinumero;
+                    $_SESSION['sukupuoli'] = $user->sukupuoli;
+                    $_SESSION['koulutus'] = $user->koulutus;
+                    $_SESSION['ohj_val'] = $user->ohj_val;    
+				    redirect(SITE_ROOT);//Palaa heti idex.php sivulle
+				} catch(PDOException $e) {
+					echo 'Käyttäjän tietojen hakuerhe';
+					file_put_contents('log/DBErrors.txt', 'tallennaProfiili 3: 
+'.$e->getMessage()."\n", FILE_APPEND);
+				}
+            }
+		} catch(PDOException $e) {
+			echo 'Tietojen lisäyserhe';
+			file_put_contents('log/DBErrors.txt', 'tallennaProfiili 2: '.$e->getMessage()."\n", 
+FILE_APPEND);
+		
+    }
+	
+
+?>
