9c8269ed4322cdcc61458040a291e59465b1d47a
blunqa <tm.blom.94@gmail.com>
2015-12-10 14:42:37 +0200

    Update login.php

--- login.php
+++ login.php
@@ -12,21 +12,30 @@ $user = login($_POST['username'], $_POST['salasana'], $DBH);
 if(!$user){
 	$_SESSION['loggausvirhe'] = 'yes';
 	//Aiheuttaa alert() pääsivulla
-    $message = "Väärä salasana tai käyttäjätunnus";
-    echo "<script type='text/javascript'>alert('$message');</script>";
-	
-    redirect(SITE_ROOT); 
+    redirect(SITE_ROOT);
 } else {
 	unset($_SESSION['loggausvirhe']);
+    
 	//Jos käyttäjätunnistettiin, talletetaan tiedot sessioon esim. kassalle siirtymistä
 	//varten on hyvä tietää asiakastiedot
 	$_SESSION['logged_in'] = 'yes';
-
-	$_SESSION['username'] = $user->username;
+    $_SESSION['username'] = $user->username;
+    $_SESSION['etunimi'] = $user->etunimi;
+	$_SESSION['sukunimi'] = $user->sukunimi;
+    $_SESSION['saika'] = $user->saika;
+	$_SESSION['puh'] = $user->puh;
+	$_SESSION['postinumero'] = $user->postinumero;
+	$_SESSION['kaupunki'] = $user->kaupunki;
+	$_SESSION['osoite'] = $user->osoite;
+	$_SESSION['sposti'] = $user->sposti;
+    $_SESSION['koulutus'] = $user->koulutus;
+    $_SESSION['sukupuoli'] = $user->sukupuoli;
+    $_SESSION['ohj_val'] = $user->ohj_val;
+    
 	//print_r($_SESSION);
 	//Jos loggaus onnistuu niin palataan paasivulle
     
-	redirect(SITE_ROOT . "/selaa.php");
+	redirect(SITE_ROOT);
 
 }
 
