f64a1c8430a2018318b075a0540504b473dd0b07
elmahone <mahonen93@gmail.com>
2015-12-10 14:35:25 +0200

    Update testprofile.php

--- testprofile.php
+++ testprofile.php
@@ -6,22 +6,27 @@ require_once('functions.php');
 	//Laitetaan syötetyt tiedot sessioon jemmaan, jotta voidaan palata muuttamaan annettuja arvoja
 	$_SESSION['lomakedata'] = serialize($data);
     //onko sähköposti oikeanlainen???
-	        echo $data['etunimi'];
-            echo '<br>';
-            echo $data['sukunimi'];
-            echo '<br>';
-            echo $data['year'].'-'.$data['month'].'-'.$data['day'];
-            echo $data['puh'];
-            echo $data['osoite'];
-            echo $data['kaupunki'];
-            echo $data['postinumero'];
-            echo $data['sukupuoli'];
-            echo $data['koulutus'];
-            echo $data['ohj_val'];
-            echo '<a href="tallennaProfile.php" class="button sininen">Tallenna</a>';
-
-  
-	
-
+    redirect(SITE_ROOT . '/tallennaProfile.php');
+//	        echo $data['etunimi'];
+//            echo '<br>';
+//            echo $data['sukunimi'];
+//            echo '<br>';
+//            echo $data['year'].'-'.$data['month'].'-'.$data['day'];
+//            echo '<br>';
+//            echo $data['puh'];
+//            echo '<br>';
+//            echo $data['osoite'];
+//            echo '<br>';
+//            echo $data['kaupunki'];
+//            echo '<br>';
+//            echo $data['postinumero'];
+//            echo '<br>';
+//            echo $data['sukupuoli'];
+//            echo '<br>';
+//            echo $data['koulutus'];
+//            echo '<br>';
+//            echo $data['ohj_val'];
+//            echo '<br>';
+//            echo '<a href="tallennaProfile.php" class="button sininen">Tallenna</a>';
 
 ?>
