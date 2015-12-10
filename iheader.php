af5638e61951aff75d06a11c0b105d7602f25b6a
elmahone <mahonen93@gmail.com>
2015-12-10 12:51:47 +0200

    Update iheader.php

--- iheader.php
+++ iheader.php
@@ -1,10 +1,12 @@
-<!doctype html>
-<html>
+<!DOCTYPE html>
+<html lang="fi">
 <head>
-    <meta charset="UTF-8">
-  <title>portfoil</title>
-    <link href="font/stylesheet.css" rel="stylesheet" type="text/css">
-	<link href="app.css" rel="stylesheet" type="text/css">
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>Verkkokauppa.net</title>
+	<link href="https://fonts.googleapis.com/css?family=Roboto&amp;subset=latin,latin-ext" rel="stylesheet" type="text/css">
+	<link href="./assets/css/fonts/foundation-icons.css" rel="stylesheet" type="text/css">
+	<link href="./assets/css/app.css" rel="stylesheet" type="text/css">
 </head>
 <body>
     <!-- Load Facebook SDK for JavaScript -->
@@ -16,27 +18,53 @@
       js.src = "//connect.facebook.net/fi_FI/sdk.js#xfbml=1&version=v2.5";
       fjs.parentNode.insertBefore(js, fjs);
     }(document, 'script', 'facebook-jssdk'));</script>
-    <div id="sivu">
-  <header>
- <a href="index.php"><img src="img/logo.png" alt="Logo"></a>
-  <div class="top-block">
+<div id="sivu">
+  <header class="tummansininen">
+    <figure class="title punainen">
+		<a href="index.php"><img src="./assets/img/logo.png" alt="Logo"></a>
+    </figure>
+      
+    <div class="top-block">
 		<nav class="top-nav">
 			<ul>
 				<li>
-					<a href="selaa.php" class="selaa">Selaa</a>
+					<a href="#" id="cart-button" class="open-modal" data-target="cart-modal">
+						<div id="cart-num" class="punainen">0</div>
+						<i class="fi-shopping-cart large"></i>
+					</a>
 				</li>
 				<li>
-					<a href="#" class="open-modal kirjaudu" data-target="log-modal">Kirjaudu</a>
-<!--                DROPDOWN MENU
-                    <ul class="dropdown">
-                        <li><a href="#">Oma profiili</a></li>
-                        <li><a href="#">Portfolio</a></li>
-                        <li><a href="#">Kirjaudu ulos</a></li>
-                    </ul>
--->
+					<a href="#" class="open-modal" data-target="log-modal">
+						<?php //Loggauduttu?
+                                //Sininen ikoni ja rasti viereen
+                               if($_SESSION['kirjautunut'] == 'jep'){ ?>
+                                  <div id="check" class="sininen">
+                                    <i class="fi-check large"></i>
+                                  </div>
+                            <?php 
+                               }?>
+
+						<i class="fi-torso large"></i>
+					</a>
 				</li>				
 			</ul>
 		</nav>
-
-      </div>
+		<form class="search-form">
+			<input class="search-field" type="text" name="haku" placeholder="Haku">
+			<button class="search-button sininen" type="submit"><i class="fi-magnifying-glass large"></i></button>
+		</form>
+    </div>
+<div style="display:none;">
+		<?php
+		//Mahdollinen kirjautumisvirhe?
+		if(isset($_SESSION['loggausvirhe']) && $_SESSION['loggausvirhe']=='jep'){	
+		   ?>
+		 <script>
+		    alert("Väärä tunnus tai salasana - yritä uudelleen");
+		 </script>
+		  <?php  
+			 unset($_SESSION['loggausvirhe']);
+        }
+		  ?>
+	</div>
   </header>
