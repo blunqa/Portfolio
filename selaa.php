8970e7738be99e561b45949550a41188444e9996
elmahone <mahonen93@gmail.com>
2015-12-11 09:58:43 +0200

    Create selaa.php

---
+++ selaa.php
@@ -0,0 +1,55 @@
+<?php include("includes/iheader.php"); ?>
+<div id="selaa-bg">  
+<h1>Uusimmat projektit</h1>
+    <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div> <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div> <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <p>Proj</p>
+            <img src="img/nainen.jpg"/>
+        </div>
+</div>
+
+        
+<?php include("includes/ifooter.php"); ?>
+        
+        
