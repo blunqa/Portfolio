a2764f84a02fed294057e2830b57d40128862728
elmahone <mahonen93@gmail.com>
2015-12-11 13:58:21 +0200

    Update and rename vprofiili.php to muokkaa_profiili.php

--- muokkaa_profiili.php
+++ muokkaa_profiili.php
@@ -2,7 +2,7 @@
 <h3>Kiitos rekisteröimisestä, <?php echo $_SESSION['username']; ?>!</h3>
 <section class="profiililomake">
   <h3>Luo profiilisi. Nämä tiedot näkyvät kaikille, jotka vierailevat sivullasi.</h3>
-<form action="<?php echo SITE_ROOT; ?>/testprofile.php" class="profiili" method="post">
+<form action="testprofile.php" class="profiili" method="post">
     <label>Etunimi*: </label><br><input class="lomake" type="text" name="data[etunimi]" required><br><br>
     <label>Sukunimi*: </label><br><input class="lomake" type="text" name="data[sukunimi]" required><br><br>
     <label>Syntymäaika*: </label><br><?php include('birth.php');?><br><br>
@@ -11,7 +11,7 @@
     <label>Kaupunki: </label><br><input class="lomake"type="text" name="data[kaupunki]"><br><br>
     <label>Puhelin: </label><br><input class="lomake" type="text" name="data[puh]"><br><br>
     <label>Sukupuoli: </label><br><select name="data[sukupuoli]">
-    		<option value="" disabled selected>Valitse</option>
+    		<option value="0" selected>Valitse</option>
             <option value="Mies">Mies</option>
             <option value="Nainen">Nainen</option>
             </select><br><br>
@@ -20,7 +20,7 @@
    <label>Käyttämäsi ohjelmisto/välineistö: </label><br><textarea rows="4" cols="50" maxlength="100" name="data[ohj_val]"></textarea><br><br>
    <label>Olen lukenut olemattomat käyttäjäehdot* </label><input type="checkbox" required><br><br>
 	<label>* Tähdellä merkityt alueet ovat pakollisia</label><br><br>
-	<input class="button_small" type='submit' value='Luo profiili' name='profiili'>
+	<input class="button_small" type='submit' value='Tallenna profiili' name='profiili'>
 
 </form>
 </section>
