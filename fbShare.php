9354373feb073bbfaf9bc9818cc64079929ac9fe
jasusetala <jasusetala@users.noreply.github.com>
2015-12-11 10:57:22 +0200

    Update fbShare.php

--- fbShare.php
+++ fbShare.php
@@ -1,8 +1,10 @@
-<div class="share-buttons">
-  <!--Facebook-->
-  <div class="fb-share-button" data-href="<?php echo 'http://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['REQUEST_URI']; ?>" data-layout="button_count">
-  </div>
-  <!--Twitter-->
-  <a href="https://twitter.com/share" class="twitter-share-button"{count}>Tweet</a>
-  <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+<div class="share-box">
+    <!--Facebook-->
+    <div class="fb-share-button" data-href="<?php echo 'http://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['REQUEST_URI']; ?>" data-layout="button_count">
+    </div>
+    <!--Twitter-->
+    <a href="https://twitter.com/share" class="twitter-share-button"{count}>Tweet</a>
+    <script>!function(d,s,id){var js,fjs=d.getElementsByTagName(s)[0],p=/^http:/.test(d.location)?'http':'https';if(!d.getElementById(id)){js=d.createElement(s);js.id=id;js.src=p+'://platform.twitter.com/widgets.js';fjs.parentNode.insertBefore(js,fjs);}}(document, 'script', 'twitter-wjs');</script>
+    <!--Url-->
+    Url: <input type="text" name="url" value="<?php echo 'http://' . $_SERVER ['HTTP_HOST'] . $_SERVER ['REQUEST_URI']; ?>" />
 </div>
