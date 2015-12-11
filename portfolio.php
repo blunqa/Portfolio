96cbfca4360d7d612c2203f466be5ad561ce71b3
elmahone <mahonen93@gmail.com>
2015-12-11 09:58:22 +0200

    Create portfolio.php

---
+++ portfolio.php
@@ -0,0 +1,53 @@
+<?php include("includes/iheader.php"); ?>
+  <section class="projektinakyma">
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div> <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div> <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/banana.gif"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <img src="img/nainen.jpg"/>
+            <p>Proj</p>
+        </div>
+        <div class="polaroid">
+            <p>Proj</p>
+            <img src="img/nainen.jpg"/>
+        </div>
+        </section>
+        </div>
+    </body>
+</html>
+<?php include("includes/ifooter.php"); ?>
