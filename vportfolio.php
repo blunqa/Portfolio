1e1be7fcb7e7042f698e185d5c8536995a9f5ca5
blunqa <tm.blom.94@gmail.com>
2015-12-14 14:11:37 +0200

    Update vportfolio.php

--- vportfolio.php
+++ vportfolio.php
@@ -3,19 +3,18 @@
 require_once('model/dbFunctions.php');
 $username = getUsername($_GET['portfolio'], $DBH); 
 ?>
-  <section class="projektinakyma">
+    
+  <section id="portfolio-bg" class="projektinakyma">
+        <h2>Oma portfoliosi</h2>
         <?php
-	   $projektit = getProjects($username->id, $DBH);
+	   $projektit = getProjects($username->uid, $DBH);
       	   foreach($projektit as $projekti){
 	     ?>
-            <a href="<?php echo SITE_ROOT; ?>/?portfolio=<?php echo $_SESSION['username']; ?>&projekti=<?php echo $projekti->id; ?>">
+            <a href="<?php echo SITE_ROOT; ?>/?portfolio=<?php echo $_SESSION['username']; ?>&projekti=<?php echo $projekti->pid; ?>">
       		<div class="polaroid">
-                <img src="img/projektikansi/<?php echo $projekti->id; ?>.jpg"/>
+                <img src="img/<?php echo $_SESSION['username']; ?>/<?php echo $projekti->nimi; ?>/projektikansi/<?php echo $projekti->pid; ?>.jpg"/>
             <p><?php echo $projekti->nimi; ?></p>
         </div>
       	   <?php
       	  } ?>
         </section>
-        </div>
-    </body>
-</html>
