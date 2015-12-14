f85fa62baa1ffff53f422eda3300409be01ff882
blunqa <tm.blom.94@gmail.com>
2015-12-14 14:12:03 +0200

    Update index.php

--- index.php
+++ index.php
@@ -5,11 +5,12 @@ require_once("functions.php");
 include("includes/iheader.php");
 SSLon();
 
-if(empty($_GET['portfolio'])) {
-include("includes/register.php");
-} else {
-    
+if(empty($_GET['portfolio']) && empty($_GET['projekti'])) {
+    include("includes/register.php");
+} else if(empty($_GET['projekti'])) {
     include('vportfolio.php');
+} else {
+    include('projekti.php');
 }
 include("includes/ifooter.php"); 
 
