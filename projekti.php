fabd81ef5d8d73778597786f94ed48842f59448f
blunqa <tm.blom.94@gmail.com>
2015-12-14 13:23:41 +0200

    Update projekti.php

--- projekti.php
+++ projekti.php
@@ -1,18 +1,27 @@
-<?php include("config.php");
-require_once("functions.php");
-include("includes/iheader.php"); 
-SSLon();?>
+<?php 
+require_once('model/dbFunctions.php');
+$project = getProject($_GET['projekti'], $DBH); 
+?>
 <div id="content">
-    <div id="tiedostot"><img src="img/nainen.jpg"></div>
+    <div id="tiedostot">
+        <?php
+	   $kuvat = getKuvat($project->pid, $DBH);
+      	   foreach($kuvat as $kuva) {
+	     ?>
+            <a href="img/<?php echo $project->username; ?>/<?php echo $project->nimi; ?>/<?php echo $kuva->kid; ?>.jpg"><img src="img/<?php echo $project->username; ?>/<?php echo $project->nimi; ?>/<?php echo $kuva->kid; ?>.jpg"></a>
+    
+    <?php
+      	  } ?>
+    </div>
     <div id="info">
-        <div class="projekti-nimi"><h1>PLACEHOLDER NIMI</h1></div>
-        <div class="projekti-pvm"> 2.2.1999</div>
-        <div class="projekti-tekija"> PLACEHOLDER TEKIJÄ</div>
-        <div class="projekti-rooli"> PLACEHOLDER ROOLI</div>
+        
+        <div class="projekti-nimi"><h1><?php echo $project->nimi; ?></h1></div>
+        <div class="projekti-pvm"><?php echo $project->pvm; ?></div>
+        <div class="projekti-tekija">Tekijä: <?php echo $project->etunimi.' '.$project->sukunimi; ?></div>
+        <div class="projekti-rooli">Rooli projektissa: <?php echo $project->rooli; ?></div>
         <div class="projekti-share"><?php include("fbShare.php"); ?></div>
-        <div class="projekti-kuvaus">PLACEHOLDER KUPLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER 
-        KUVAUS PLACEHOLDER KUVAUS LDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS </div>
+        <div class="projekti-kuvaus"><h3>Projektin kuvaus</h3><br><?php echo $project->kuvaus; ?></div>
     </div>
 
 </div>
-<?php include("includes/ifooter.php"); ?>
+
