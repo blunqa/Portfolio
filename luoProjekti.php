b91d7a59e7be8cbb5d6f6dfd18f1c5af3b47fd92
elmahone <mahonen93@gmail.com>
2015-12-14 09:48:03 +0200

    Update luoProjekti.php

--- luoProjekti.php
+++ luoProjekti.php
@@ -1,16 +1,26 @@
-<?php include("includes/iheader.php"); ?>
-<h3>Luo projekti</h3>
-<form class="profiili">
-    Projektin nimi*: <br><input class="lomake" type="text" name="pr_nimi" required>
-    <br><br>
-    Kuvaus*: <br><textarea rows="4" cols="50" maxlength="500" name="pr_kuvaus" required></textarea>
-    <br><br>
-    Roolisi projektissa: <br><input class="lomake" name="pr_rooli" ></textarea>
-    <br><br>
-    Projektin kansikuva*: <br><input type="file" name="fileToUpload" id="fileToUpload" required>
-    <br><br>
-    <input type="submit" value="Luo projekti" name="luoprojekti">
-    <br><br>
-    <p>* Tähdellä merkityt alueet ovat pakollisia</p>
-</form>
+<?php 
+include("config.php");
+require_once("functions.php");
+include("includes/iheader.php");
+?>
+<div id="luoProjekti-bg">
+    <section class="profiililomake">
+    <h3>Luo projekti</h3>
+    <br>
+        <form action="upload.php" class="projekti" method="post">
+            Projektin nimi*: <br><input class="lomake" type="text" name="pr_nimi" required>
+            <br><br>
+            Kuvaus*: <br><textarea rows="4" cols="53"  name="pr_kuvaus" required></textarea>
+            <br><br>
+            Roolisi projektissa: <br><input class="lomake" name="pr_rooli" ></textarea>
+            <br><br>
+            Projektin kansikuva*: <br><input type="file" name="fileToUpload" id="fileToUpload" required>
+            <br><br>
+            <input class="button_small" type="submit" value="Luo projekti" name="luoprojekti">
+            <br><br>
+            <label>* Tähdellä merkityt alueet ovat pakollisia</label>
+        </form>
+    </section>
+</div>
+
 <?php include("includes/ifooter.php"); ?>
