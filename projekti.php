192bdd1099a58b1120e6e8655c8cee8c13f15051
elmahone <mahonen93@gmail.com>
2015-12-14 11:49:00 +0200

    Update projekti.php

--- projekti.php
+++ projekti.php
@@ -1,13 +1,17 @@
-<?php include("includes/iheader.php"); ?>
+<?php include("config.php");
+require_once("functions.php");
+include("includes/iheader.php"); 
+SSLon();?>
 <div id="content">
-    <div id="tiedostot"><h1>Projektin tiedostot</h1><br><img src="uploads/2.jpg"></div>
+    <div id="tiedostot"><img src="img/nainen.jpg"></div>
     <div id="info">
         <div class="projekti-nimi"><h1>PLACEHOLDER NIMI</h1></div>
-        <div class="projekti-pvm">2.2.1999</div>
+        <div class="projekti-pvm"> 2.2.1999</div>
         <div class="projekti-tekija"> PLACEHOLDER TEKIJÄ</div>
         <div class="projekti-rooli"> PLACEHOLDER ROOLI</div>
-        <div class="projekti-share">FB | TWTR | G+</div>
-        <div class="projekti-kuvaus">PLACEHOLDER KUVAUS</div>
+        <div class="projekti-share"><?php include("fbShare.php"); ?></div>
+        <div class="projekti-kuvaus">PLACEHOLDER KUPLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER 
+        KUVAUS PLACEHOLDER KUVAUS LDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS PLACEHOLDER KUVAUS </div>
     </div>
 
 </div>
