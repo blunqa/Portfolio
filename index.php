3ef6c1a707b1b963d24458c584b3f4c396a4711c
blunqa <tm.blom.94@gmail.com>
2015-12-14 10:32:44 +0200

    Update index.php

--- index.php
+++ index.php
@@ -1,17 +1,16 @@
-<?php include("includes/iheader.php"); ?>
-    <div class="esittely">
-        <div class="esittely-bg">
-       <div class="esittely-text">
-        <h1>Tervetuloa!</h1>
-           <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi. </br><b>Rekisteröidy jo tänään!</b></p>
-        </div><div class="esittely-reg">
-        <form class="reg-form">
-			<input class="reg-field lomake" type="text" name="user" placeholder="Username">
-            <input class="reg-field lomake" type="text" name="email" placeholder="Email">
-            <input class="reg-field lomake" type="password" name="pass" placeholder="Password">
-        </form>
-        <input type="submit" value="Rekisteröidy" id="reg">
-        </div>
-    </div>
-        </div>
-<?php include("includes/ifooter.php"); ?>
+<?php 
+
+include("config.php");
+require_once("functions.php");
+include("includes/iheader.php");
+SSLon();
+
+if(empty($_GET['portfolio'])) {
+include("includes/register.php");
+} else {
+    
+    include('vportfolio.php');
+}
+include("includes/ifooter.php"); 
+
+?>
