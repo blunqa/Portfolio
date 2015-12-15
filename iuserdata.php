0938b1ec9ebf10457301037491361a18af892817
blunqa <tm.blom.94@gmail.com>
2015-12-15 13:06:52 +0200

    Update iuserdata.php

--- iuserdata.php
+++ iuserdata.php
@@ -1,57 +1,64 @@
-	<div class="tiedot">   
-	<h1>Käyttäjän <?php echo $_SESSION['username']?> profiili</h1>
+<?php require_once('model/dbFunctions.php');
+$username = getUsername($_GET['username'], $DBH);
+?>
+<div class="tiedot">   
+	<h1>Käyttäjän <?php echo $username->username; ?> profiili</h1>
         <br>
-        <br>
+    <a href="<?php echo SITE_ROOT.'?portfolio='.$username->username; ?>">
+    <input type="submit" class="button_small" value="Portfolio">
+    </a>
+    <br>
+    <br>
     <?php // Jos nimi on kerrottu se näytetään
-        if(($_SESSION['etunimi'])!=NULL || $_SESSION['sukunimi'] != NULL){
+        if($username->etunimi !=NULL || $username->sukunimi != NULL){
         echo '<div class="tieto-container">';?>
         <h3>Nimi</h3> 
-        <p><?php echo $_SESSION['etunimi'].' '.$_SESSION['sukunimi'];?></p>
+        <p><?php echo $username->etunimi.' '.$username->sukunimi;?></p>
             <?php echo '</div>';}?>
     <?php // Jos sukupuoli on kerrottu se näytetään
-        if(($_SESSION['sukupuoli']!=0 || $_SESSION['sukupuoli'])!=NULL){
+        if($username->sukupuoli !=0 || $username->sukupuoli !=NULL){
         echo '<div class="tieto-container">';?>
             <h3>Sukupuoli</h3>
         <?php 
                 echo '<p>';
-                echo $_SESSION['sukupuoli'];
+                echo $username->sukupuoli;
                 echo '</p>';
                 echo '</div>';}?>
         
     <div class="tieto-container">
         <h3>Sähköposti</h3>
-        <p><?php echo $_SESSION['sposti'];?></p>
+        <p><?php echo $username->sposti;?></p>
     </div>
     <?php // Jos puhelinnumero on kerrottu se näytetään
-        if(($_SESSION['puh'])!=NULL){
+        if($username->puh !=NULL){
         echo '<div class="tieto-container">';?>
                 <h3>Puhelinnumero</h3>
                 <?php 
                     echo '<p>';
-                    echo $_SESSION['puh'];
+                    echo $username->puh;
                     echo '</p>';
                     echo '</div>';}?>
             
      <?php // Jos Osoite on kerrottu se näytetään
-        if($_SESSION['osoite'] !=NULL || $_SESSION['postinumero'] != NULL || $_SESSION['kaupunki'] != NULL){
+        if($username->osoite !=NULL || $username->postinumero != NULL || $username->kaupunki != NULL){
         echo '<div class="tieto-container">';?>
         <h3>Osoite</h3>
         <?php
             echo '<p>';
-            if($_SESSION['osoite'] !=NULL) { 
-            echo $_SESSION['osoite'].' ';
+            if($username->osoite !=NULL) { 
+            echo $username->osoite .' ';
             } 
-            if ($_SESSION['postinumero'] !=NULL) {
-            echo $_SESSION['postinumero'].' ';
+            if ($username->postinumero  !=NULL) {
+            echo $username->postinumero.' ';
             } 
-            if ($_SESSION['kaupunki'] !=NULL){
-            echo $_SESSION['kaupunki'];
+            if ($username->kaupunki !=NULL){
+            echo $username->kaupunki;
             };
             echo '</p>';
             echo '</div>';}?>
         
     <?php // Jos syntymäpäivä on kerrottu se näytetään ikänä    
-        if($_SESSION['saika'] !="0000-00-00"){
+        if($username->pvm !="0000-00-00"){
         echo '<div class="tieto-container">';?>
         <h3>Ikä</h3>
         <?php 
@@ -61,27 +68,29 @@
             echo '</div>';}?>
     
         <?php // Jos koulutus on kerrottu se näytetään
-        if($_SESSION['koulutus'] != NULL){
+        if($username->koulutus != NULL){
         echo '<div class="tieto-container">';?>
         <h3>Koulutus</h3>
         <?php 
             echo '<p>';
-            echo $_SESSION['koulutus'];
+            echo $username->koulutus;
             echo '</p>';
             echo '</div>';}?>
     
     <?php // Jos Käytetty Ohjelmisto/Välineistö on kerrottu se näytetään
-        if($_SESSION['ohj_val'] != NULL){
+        if($username->ohj_val != NULL){
         echo '<div class="tieto-container">';?>
     
         <h3>Käytetty Ohjelmisto/Välineistö</h3>
         <?php 
             echo '<p>';
-            echo $_SESSION['ohj_val'];
+            echo $username->ohj_val;
             echo '</p>';
             echo '</div>';}?>
-        <form action="luoprofiili.php" class="profiili" method="post">
-            <input class="button_small" type='submit' action="luoprofiili.php" value='Muokkaa Profiilia' name='profiili'>
+        <?php if($_SESSION['username'] == $username->username) { ?>
+        <form action="luoProfiili.php" class="profiili" method="post">
+            <input class="button_small" type='submit' action="luoProfiili.php" value='Muokkaa Profiilia' name='profiili'>
         </form>
+    <?php } ?>
 </div>
 
