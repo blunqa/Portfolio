e64b1fea5c81b730820111d761719a36ff5bcd82
elmahone <mahonen93@gmail.com>
2015-12-16 13:19:27 +0200

    Update yhteystiedot.php

--- yhteystiedot.php
+++ yhteystiedot.php
@@ -2,6 +2,30 @@
 include("config.php");
 require_once("functions.php");
 include("includes/iheader.php");
-include("tiedot.php");
+<div id="yhteystiedot-bg">
+    <div class="tiedot">
+        <br>
+        <h1>Lumnium</h1>
+        <br>
+        <br>
+        <h2>Osoite</h2>
+        <h3>Vanha maantie 6</h3>
+        <h3>02650 Espoo</h3>
+        <br>
+        <br>
+        <h2>Puhelin</h2>
+        <h3>020202</h3>
+        <br>
+        <br>
+        <h3>asiakaspalvelu@lumnium.fi</h3>
+        
+    </div>
+    <div class="sijainti">
+        <h1>Lumnium sijainti</h1>
+        <br>
+        <div class="kartta"><iframe width="470" height="305" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Vanha%20maantie%206%2C%20Espoo%2C%20Suomi&key=AIzaSyAq6dx2lNFU7LOmLj_4nU2Pacf01KGsgdY" allowfullscreen></iframe></div>
+    </div>
+</div>
+
 
 include("includes/ifooter.php"); ?>
