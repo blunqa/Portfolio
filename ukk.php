fae405b7666b32647674c07708b08003e9d8e819
jasusetala <jasusetala@users.noreply.github.com>
2015-12-16 13:23:52 +0200

    Update ukk.php

--- ukk.php
+++ ukk.php
@@ -12,28 +12,28 @@ include('includes/iheader.php');
     <!-- Ei kuitenkaan riko muilla selaimilla. Näyttää vain kuin 'details' ja 'summary' -tageja ei olisi. -->
     <details>
         <summary>
-            <h2 style="display: inline;">Mikä on Lumnium?</h2>
+            <h2>Mikä on Lumnium?</h2>
         </summary>
         <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi.</p>
     </details>    
     <br>
     <details>
         <summary>
-            <h2 style="display: inline;">Kuka voi käyttää Lumniumia?</h2>
+            <h2>Kuka voi käyttää Lumniumia?</h2>
         </summary>
         <p>Lumniumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi <a href="index.php">etusivulta</a>.</p>
     </details>    
     <br>
     <details>
         <summary>
-            <h2 style="display: inline;">Mikä on projekti?</h2>
+            <h2>Mikä on projekti?</h2>
         </summary>    
         <p>Käyttäjät voivat luoda portfolioonsa useita projekteja. Projektiin voidaan lisätä kuvia, tekijän rooli ja kuvaus.</p>
     </details>    
     <br>
     <details>
         <summary>
-            <h2 style="display: inline;">Kuinka luon uuden projektin?</h2>
+            <h2>Kuinka luon uuden projektin?</h2>
         </summary>
         <p>Kun olet kirjautuneena sisään klikkaa käyttäjätunnustasi oikeasta yläkulmasta ja valitse tiputusvalikosta "Luo projekti".</p>
     </details>
