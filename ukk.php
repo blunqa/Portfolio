c84a618c838e661b43ad3dfb3cc9ba647ad83f8e
jasusetala <jasusetala@users.noreply.github.com>
2015-12-16 11:12:54 +0200

    Update ukk.php

--- ukk.php
+++ ukk.php
@@ -12,7 +12,7 @@ include('includes/iheader.php');
     <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi.</p>
     <br>
     <h2>Kuka voi käyttää Luminiumia?</h2>
-    <p>Luminiumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi etusivulta.</p>
+    <p>Luminiumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi <a href="index.php">etusivulta</a>.</p>
     <br>
     <h2>Mikä on projekti?</h2>
     <p>Käyttäjät voivat luoda portfolioonsa useita projekteja. Projektiin voidaan lisätä kuvia, tekijän rooli ja kuvaus.</p>
