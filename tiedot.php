354d0eaf83ee25b16152063a0ed31ddb99094073
elmahone <mahonen93@gmail.com>
2015-12-16 10:24:42 +0200

    Create tiedot.php

---
+++ tiedot.php
@@ -0,0 +1,17 @@
+<div id="yhteystiedot-bg">
+    <div class="tiedot">
+        <h1>Lumnium</h1>
+        <br>
+        <h3>Vanha maantie 6</h3>
+        <br>
+        <h3>02650 Espoo</h3>
+        <br>
+        <h3>020202</h3>
+        
+    </div>
+    <div class="sijainti">
+        <h1>Lumnium sijainti</h1>
+        <br>
+        <div class="kartta"><iframe width="470" height="305" frameborder="0" style="border:0" src="https://www.google.com/maps/embed/v1/place?q=Vanha%20maantie%206%2C%20Espoo%2C%20Suomi&key=AIzaSyAq6dx2lNFU7LOmLj_4nU2Pacf01KGsgdY" allowfullscreen></iframe></div>
+    </div>
+</div>
