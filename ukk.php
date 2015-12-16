ab5b823ff602b627facfb212e636dd729e9f0d30
jasusetala <jasusetala@users.noreply.github.com>
2015-12-16 12:44:28 +0200

    Update ukk.php

--- ukk.php
+++ ukk.php
@@ -7,18 +7,35 @@ SSLon();
 include('includes/iheader.php');
 ?>
 <div id="selaa-bg">
-    <h1>Usein kysytyt kysymykset (FAQ)</h1>  
-    <h2>Mikä on Lumnium?</h2>
-    <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi.</p>
+    <h1>Usein kysytyt kysymykset (FAQ)</h1>
+    <!-- 'details' ja 'summary' toimivat vain selaimilla: Chrome 12.0+, Safari 6.0+ ja Opera 15.0+ -->
+    <details>
+        <summary>
+            <h2 style="display: inline;">Mikä on Lumnium?</h2>
+        </summary>
+        <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi.</p>
+    </details>    
     <br>
-    <h2>Kuka voi käyttää Lumniumia?</h2>
-    <p>Lumniumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi <a href="index.php">etusivulta</a>.</p>
+    <details>
+        <summary>
+            <h2 style="display: inline;">Kuka voi käyttää Lumniumia?</h2>
+        </summary>
+        <p>Lumniumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi <a href="index.php">etusivulta</a>.</p>
+    </details>    
     <br>
-    <h2>Mikä on projekti?</h2>
-    <p>Käyttäjät voivat luoda portfolioonsa useita projekteja. Projektiin voidaan lisätä kuvia, tekijän rooli ja kuvaus.</p>
+    <details>
+        <summary>
+            <h2 style="display: inline;">Mikä on projekti?</h2>
+        </summary>    
+        <p>Käyttäjät voivat luoda portfolioonsa useita projekteja. Projektiin voidaan lisätä kuvia, tekijän rooli ja kuvaus.</p>
+    </details>    
     <br>
-    <h2>Kuinka luon uuden projektin?</h2>
-    <p>Kun olet kirjautuneena sisään klikkaa käyttäjätunnustasi oikeasta yläkulmasta ja valitse tiputusvalikosta "Luo projekti".</p>
+    <details>
+        <summary>
+            <h2 style="display: inline;">Kuinka luon uuden projektin?</h2>
+        </summary>
+        <p>Kun olet kirjautuneena sisään klikkaa käyttäjätunnustasi oikeasta yläkulmasta ja valitse tiputusvalikosta "Luo projekti".</p>
+    </details>
     <br>
 </div>
 <?php include('includes/ifooter.php'); ?>
