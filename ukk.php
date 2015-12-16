a0dac2d47143e58065fb7a93aa90ce9b55347770
jasusetala <jasusetala@users.noreply.github.com>
2015-12-16 10:52:04 +0200

    Create ukk.php

---
+++ ukk.php
@@ -0,0 +1,24 @@
+<?php 
+require_once('config.php');
+require_once('functions.php');
+//require_once('dbFunctions.php');
+SSLon();
+
+include('includes/iheader.php');
+?>
+<div id="selaa-bg">
+    <h1>Usein kysytyt kysymykset (FAQ)</h1>  
+    <h2>Mikä on Luminium?</h2>
+    <p>Lumnium on helppokäyttöinen julkaisupalvelu, johon käyttäjät voivat luoda oman yksityiskohtaisen profiilin sekä ladata omia tuotoksiaan muiden nähtäväksi.</p>
+    <br>
+    <h2>Kuka voi käyttää Luminiumia?</h2>
+    <p>Luminiumia voivat käyttää kaikki rekisteröityneet käyttäjät. Rekisteröityä voi etusivulta.</p>
+    <br>
+    <h2>Mikä on projekti?</h2>
+    <p>Käyttäjät voivat luoda portfolioonsa useita projekteja. Projektiin voidaan lisätä kuvia, tekijän rooli ja kuvaus.</p>
+    <br>
+    <h2>Kuinka luon uuden projektin?</h2>
+    <p>Kun olet kirjautuneena sisään klikkaa käyttäjätunnustasi oikeasta yläkulmasta ja valitse tiputusvalikosta "Luo projekti".</p>
+    <br>
+</div>
+<?php include('includes/ifooter.php'); ?>
